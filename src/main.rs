6e3878c97a0f1d0b5150ef303e0cdf17ea00c3ad
Kat-Luk <katarinalukacova6@gmail.com>
2026-06-13 09:26:08 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+pub mod map;
+use std::io;
+use crate::map::arraymap::*;
+fn main() {
+    let mut row: usize = 2;
+    let mut column: usize = 2;
+    let mut map = create_map();
+    print_map(&mut map); 
+    loop {
+        println!("| w) Move up | a) Move left | s) Move down | d) Move right | e) End program |");
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice).expect("Error reading input.");
+        let choice = choice.trim();
+        match choice {
+            "e" => {
+                println!("Ending the program.");
+                break;
+            }
+            "a" => {
+                move_left(&mut map, &mut row, &mut column);
+                print_map(&mut map); 
+            }
+            "s" => {
+                move_down(&mut map, &mut row, &mut column);
+                print_map(&mut map); 
+            }
+            "d" => {
+                move_right(&mut map, &mut row, &mut column);
+                print_map(&mut map); 
+            }
+            "w" => {
+                move_up(&mut map, &mut row, &mut column);
+                print_map(&mut map); 
+            }
+            _ => {
+                println!("Wrong input")
+            }
+        }
+    }
+}
---
+++ src/map/arraymap.rs
@@ -0,0 +1,52 @@
+pub fn create_map() -> [[&'static str; 5]; 5]{
+    let mut map = [
+        ["*","*","*","*","*"],
+        ["*","*","*","*","*"],
+        ["*","*","X","*","*"],
+        ["*","*","*","*","*"],
+        ["*","*","*","*","*"]
+    ];
+    return map;
+}
+
+pub fn print_map(map: &mut [[&str; 5]; 5]) {
+    for row in map.iter() {
+        for column in row.iter() {
+            print!("{}", column);
+        }
+        println!();
+    }
+}
+
+pub fn move_up(map: &mut [[&str; 5]; 5], row: &mut usize, column: &mut usize) {
+    if *row != 0{
+        map[*row][*column] = "*";
+        *row -= 1;
+        map[*row][*column] = "X";
+    } else {println!("Can't move out of the map");}
+}
+
+pub fn move_down(map: &mut [[&str; 5]; 5], row: &mut usize, column: &mut usize) {
+    if *row != 4{
+        map[*row][*column] = "*";
+        *row += 1;
+        map[*row][*column] = "X";
+    } else {println!("Can't move out of the map");}
+}
+
+pub fn move_right(map: &mut [[&str; 5]; 5], row: &mut usize, column: &mut usize) {
+    if *column != 4{
+        map[*row][*column] = "*";
+        *column += 1;
+        map[*row][*column] = "X";
+    } else {println!("Can't move out of the map");}
+}
+
+pub fn move_left(map: &mut [[&str; 5]; 5], row: &mut usize, column: &mut usize) {
+    if *column != 0{
+        map[*row][*column] = "*";
+        *column -= 1;
+        map[*row][*column] = "X";
+    } else {println!("Can't move out of the map");}
+}
+
