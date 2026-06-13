53d310544b5e3f290c654b165e28face552104cb
Kat-Luk <katarinalukacova6@gmail.com>
2026-06-13 14:51:44 +0200

    Refactor map dimensions to 5x5 and adjust related functions for consistency

--- src/map/arraymap.rs
+++ src/map/arraymap.rs
@@ -1,24 +1,27 @@
-pub fn create_map() -> [[char; 9]; 5]{
+pub fn create_map() -> [[char; 5]; 5]{
     let map = [
-        ['*', ' ', '*', ' ', '*', ' ', '*', ' ', '*'],
-        ['*', ' ', '*', ' ', '*', ' ', '*', ' ', '*'],
-        ['*', ' ', '*', ' ', 'x', ' ', '*', ' ', '*'],
-        ['*', ' ', '*', ' ', '*', ' ', '*', ' ', '*'],
-        ['*', ' ', '*', ' ', '*', ' ', '*', ' ', '*']
+        ['*', '*', '*', '*', '*'],
+        ['*', '*', '*', '*', '*'],
+        ['*', '*', 'x', '*', '*'],
+        ['*', '*', '*', '*', '*'],
+        ['*', '*', '*', '*', '*']
     ];
     return map;
-}
+} 
 
-pub fn print_map(map: &mut [[char; 9]; 5]) {
+pub fn print_map(map: &[[char; 5]; 5]) {
     for row in map.iter() {
-        for column in row.iter() {
+        for (i,column) in row.iter().enumerate() {
+            if i > 0 {
+                print!(" ");
+            }
             print!("{}", column);
         }
         println!();
     }
 }
 
-pub fn move_up(map: &mut [[char; 9]; 5], row: &mut usize, column: &mut usize) {
+pub fn move_up(map: &mut [[char; 5]; 5], row: &mut usize, column: &mut usize) {
     if *row != 0 {
         map[*row][*column] = '*';
         *row -= 1;
@@ -26,7 +29,7 @@ pub fn move_up(map: &mut [[char; 9]; 5], row: &mut usize, column: &mut usize) {
     } else {println!("Can't move out of the map");}
 }
 
-pub fn move_down(map: &mut [[char; 9]; 5], row: &mut usize, column: &mut usize) {
+pub fn move_down(map: &mut [[char; 5]; 5], row: &mut usize, column: &mut usize) {
     if *row != 4{
         map[*row][*column] = '*';
         *row += 1;
@@ -34,18 +37,18 @@ pub fn move_down(map: &mut [[char; 9]; 5], row: &mut usize, column: &mut usize) 
     } else {println!("Can't move out of the map");}
 }
 
-pub fn move_right(map: &mut [[char; 9]; 5], row: &mut usize, column: &mut usize) {
-    if  *column != 8 {
+pub fn move_right(map: &mut [[char; 5]; 5], row: &mut usize, column: &mut usize) {
+    if  *column != 4 {
         map[*row][*column] = '*';
-        *column += 2;
+        *column += 1;
         map[*row][*column] = 'x';
     } else {println!("Can't move out of the map");}
 }
 
-pub fn move_left(map: &mut [[char; 9]; 5], row: &mut usize, column: &mut usize) {
+pub fn move_left(map: &mut [[char; 5]; 5], row: &mut usize, column: &mut usize) {
     if *column != 0{
         map[*row][*column] = '*';
-        *column -= 2;
+        *column -= 1;
         map[*row][*column] = 'x';
     } else {println!("Can't move out of the map");}
 }
