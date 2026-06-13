d4f88f605af3c272fccbf9e6529d59ac6eaa4961
Kat-Luk <katarinalukacova6@gmail.com>
2026-06-13 09:30:32 +0200

    Refactor map representation to include spaces for better formatting

--- src/map/arraymap.rs
+++ src/map/arraymap.rs
@@ -1,10 +1,10 @@
 pub fn create_map() -> [[&'static str; 5]; 5]{
     let mut map = [
-        ["*","*","*","*","*"],
-        ["*","*","*","*","*"],
-        ["*","*","X","*","*"],
-        ["*","*","*","*","*"],
-        ["*","*","*","*","*"]
+        ["* ","* ","* ","* ","*"],
+        ["* ","* ","* ","* ","*"],
+        ["* ","* ","X ","* ","*"],
+        ["* ","* ","* ","* ","*"],
+        ["* ","* ","* ","* ","*"]
     ];
     return map;
 }
@@ -20,33 +20,33 @@ pub fn print_map(map: &mut [[&str; 5]; 5]) {
 
 pub fn move_up(map: &mut [[&str; 5]; 5], row: &mut usize, column: &mut usize) {
     if *row != 0{
-        map[*row][*column] = "*";
+        map[*row][*column] = "* ";
         *row -= 1;
-        map[*row][*column] = "X";
+        map[*row][*column] = "X ";
     } else {println!("Can't move out of the map");}
 }
 
 pub fn move_down(map: &mut [[&str; 5]; 5], row: &mut usize, column: &mut usize) {
     if *row != 4{
-        map[*row][*column] = "*";
+        map[*row][*column] = "* ";
         *row += 1;
-        map[*row][*column] = "X";
+        map[*row][*column] = "X ";
     } else {println!("Can't move out of the map");}
 }
 
 pub fn move_right(map: &mut [[&str; 5]; 5], row: &mut usize, column: &mut usize) {
     if *column != 4{
-        map[*row][*column] = "*";
+        map[*row][*column] = "* ";
         *column += 1;
-        map[*row][*column] = "X";
+        map[*row][*column] = "X ";
     } else {println!("Can't move out of the map");}
 }
 
 pub fn move_left(map: &mut [[&str; 5]; 5], row: &mut usize, column: &mut usize) {
     if *column != 0{
-        map[*row][*column] = "*";
+        map[*row][*column] = "* ";
         *column -= 1;
-        map[*row][*column] = "X";
+        map[*row][*column] = "X ";
     } else {println!("Can't move out of the map");}
 }
 
